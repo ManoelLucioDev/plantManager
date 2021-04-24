9f37da83042a1a9281a09aeb8a744213ed6ed4ec
MANOEL <manoel.programer@gmail.com>
2021-04-24 00:37:23 -0300

    Aula 3 e 4

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation, useRoute } from '@react-navigation/core';
 import React from 'react';
 import {
     SafeAreaView,
@@ -11,25 +12,53 @@ import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params{
+    title: string;
+    subtitle: string;
+    buttonTitle: string;
+    icon: 'smile' | 'hug';
+    nextScreen: string;
+}
+
+const emojis = {
+    hug: '🤪',
+    smile: '😄'
+}
+
 export function Confirmation(){
+    const navigation = useNavigation();
+    const routes = useRoute();
+
+    const {
+        title,
+        subtitle,
+        buttonTitle,
+        icon,
+        nextScreen
+    } = routes.params as Params;
+
+    function handleMoveOn(){
+        navigation.navigate(nextScreen);
+    }
     return(
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.emoji}>
-                    😄
+                    {emojis[icon]}
                 </Text>
 
                 <Text style={styles.title}>
-                    Prontinho
+                    {title}
                 </Text>
 
                 <Text style={styles.subtitle}>
-                    Agora vamos cuidar das suas plantinhas com muito cuidado.
+                    {subtitle}
                 </Text>
 
                 <View style={styles.footer}>
                     <Button 
-                        title= 'Começar'
+                        title= {buttonTitle}
+                        onPress={handleMoveOn}
                     />
                 </View>
             </View>
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import colors from '../styles/colors';
+
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSave } from '../pages/PlantSave';
+import { MyPlants } from '../pages/MyPlants';
+
+import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -30,6 +35,21 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen 
             name='Confirmation'
             component={Confirmation}
+        />
+
+        <stackRoutes.Screen 
+            name="PlantSelect"
+            component={AuthRoutes}
+        />
+
+        <stackRoutes.Screen 
+            name="PlantSave"
+            component={PlantSave}
+        />
+
+        <stackRoutes.Screen 
+            name="MyPlants"
+            component={AuthRoutes}
         />
 
     </stackRoutes.Navigator>
