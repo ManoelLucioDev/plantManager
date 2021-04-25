a554c0d7ca4c5a03a65b3c46af55faffab500dd7
MANOEL <manoel.programer@gmail.com>
2021-04-25 18:03:07 -0300

    Implementação de bom dia, boa tarde ou boa noite, de forma autmomatica

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,70 +1,75 @@
-import React, { useEffect, useState} from 'react';
-import {
-    StyleSheet,
-    Image,
-    Text,
-    View
-} from 'react-native';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, Text, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
+import colors from "../styles/colors";
+import userImg from "../assets/foto.png";
+import fonts from "../styles/fonts";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import colors from '../styles/colors';
-import userImg from '../assets/foto.png';
-import fonts from '../styles/fonts';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+export function Header() {
+  const [userName, setUserName] = useState<string>();
+  
 
+  useEffect(() => {
+    async function loadStorageUserName() {
+      const user = await AsyncStorage.getItem("@plantmanager:user");
+      setUserName(user || "");
+    }
 
-export function Header(){
-    const [userName, setUserName] = useState<string>();
+    loadStorageUserName();
+  }, []);
 
-    useEffect(() => {
-        async function loadStorageUserName(){
-            const user = await AsyncStorage.getItem('@plantmanager:user');
-            setUserName(user || '');
-        }
+  var today = new Date();
+  var hourNow = today.getHours();
 
-        loadStorageUserName();
-    },[]);
+  var msg;
 
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.greeting}>Olá,</Text>
-                <Text style={styles.userName}>
-                            {userName}
-                </Text>
-            </View>
+  if (hourNow > 18) {
+    msg = "Boa noite, ";
+  } else if (hourNow > 12) {
+    msg = "Boa tarde, ";
+  } else if (hourNow > 0) {
+    msg = "Bom dia";
+  } else {
+    msg = "Seja bem-vindo! ";
+  }
 
-            <Image source={userImg} style={styles.image} />
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>{msg}</Text>
+        <Text style={styles.userName}>{userName}</Text>
+      </View>
 
-        </View>
-    )
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
 }
 
-const styles= StyleSheet.create({
-    container:{
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent:  'space-between',
-        alignItems: 'center',
-        paddingVertical: 20,
-        marginTop: getStatusBarHeight()
-    },
-    greeting:{
-        fontSize: 32,
-        color: colors.heading,
-        fontFamily: fonts.text
-    },
-    userName:{
-        fontSize: 32,
-        fontFamily: fonts.heading,
-        color: colors.heading,
-        lineHeight: 40
-    },
-    image:{
-        width: 70,
-        height: 70,
-        borderRadius: 40,
-        
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+});
